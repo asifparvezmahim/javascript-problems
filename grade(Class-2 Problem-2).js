217623d93f32e79dd0b25cab676196f4148165f8
Md.Asif Parvez Mahim <100875772+asifparvezmahim@users.noreply.github.com>
2022-08-01 19:23:48 +0600

    forth-commit

---
+++ grade(Class-2 Problem-2).js
@@ -0,0 +1,125 @@
+/*
+You and your friend Tom,Jenny,Peter and John got their final exam results.Your total score is 85, Tom's total score is 66 , Jenny's scoor is 95, Peters score is 56 and John's scoor is 40.
+
+The grade List is: 
+    80 or above A Grade
+    60 or above B Grade
+    50 or above C Grade
+    40 or above D Grade
+    39 or less => f Grade
+*/
+
+var Asif = 85;
+var Tom = 66;
+var Jeny = 95;
+var Peter = 56;
+var John = 40;
+
+//Asif's Grade
+if (Asif >= 80) {
+    console.log("Asif's Grade is : A");
+}
+
+else if (Asif >= 60) {
+    console.log("Asif's Grade is : B");
+}
+
+else if (Asif >= 50) {
+    console.log("Asif's Grade is : C");
+}
+
+else if (Asif >= 40) {
+    console.log("Asif's Grade is : D");
+}
+
+else {
+    console.log("Asif's Grade is : F");
+}
+
+
+//Tom's Grade
+
+if (Tom >= 80) {
+    console.log("Tom's Grade is : A");
+}
+
+else if (Tom >= 60) {
+    console.log("Tom's Grade is : B");
+}
+
+else if (Tom >= 50) {
+    console.log("Tom's Grade is : C");
+}
+
+else if (Tom >= 40) {
+    console.log("Tom's Grade is : D");
+}
+
+else {
+    console.log("Tom's Grade is : F");
+}
+
+//Jeny's Grade
+
+if (Jeny >= 80) {
+    console.log("Jeny's Grade is : A");
+}
+
+else if (Jeny >= 60) {
+    console.log("Jeny's Grade is : B");
+}
+
+else if (Jeny >= 50) {
+    console.log("Jeny's Grade is : C");
+}
+
+else if (Jeny >= 40) {
+    console.log("Jeny's Grade is : D");
+}
+
+else {
+    console.log("Jeny's Grade is : F");
+}
+
+//Peter's Grade
+if (Peter >= 80) {
+    console.log("Peter's Grade is : A");
+}
+
+else if (Peter >= 60) {
+    console.log("Peter's Grade is : B");
+}
+
+else if (Peter >= 50) {
+    console.log("Peter's Grade is : C");
+}
+
+else if (Peter >= 40) {
+    console.log("Peter's Grade is : D");
+}
+
+else {
+    console.log("Peter's Grade is : F");
+}
+
+//John's Grade
+
+if (John >= 80) {
+    console.log("John's Grade is : A");
+}
+
+else if (John >= 60) {
+    console.log("John's Grade is : B");
+}
+
+else if (John >= 50) {
+    console.log("John's Grade is : C");
+}
+
+else if (John >= 40) {
+    console.log("John's Grade is : D");
+}
+
+else {
+    console.log("John's Grade is : F");
+}
