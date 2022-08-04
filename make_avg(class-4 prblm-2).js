a3c59eaef77ef7bc00d87c62fc1bf26721c9dd16
Md.Asif Parvez Mahim <100875772+asifparvezmahim@users.noreply.github.com>
2022-08-04 15:29:18 +0600

    sixth-commit

---
+++ make_avg(class-4 prblm-2).js
@@ -0,0 +1,7 @@
+function make_avg(num1, num2, num3) {
+    var avg = (num1 + num2 + num3) / 3;
+    return avg;
+}
+
+var result = make_avg(2, 8, 1);
+console.log("The Average Result is : ", result);
